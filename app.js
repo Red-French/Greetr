3bab52ddd36b44ebe97943ff02f99fd66eb66fd2
Red-French <red@redfrench.net>
2020-01-22 21:15:17 -0600

    finish

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 const g = G$('John', 'Wayne');
-console.log(g);
+
+console.log("object: ", g);
+console.log("--------");
+
+g.greet().greet(true);  // informal followed by formal greeting
+console.log("--------");
+
 g.greet().setLang('es').greet("formal");
+console.log("--------");
+
 g.setLang('en').greet().log();
+console.log("--------");
+
+g.setLang('es').greet();
+
+$("#login").click(function login() {
+  const firstname = $("#firstname").val();
+  const lastname = $("#lastname").val();
+  const formality = $("#formality").is(":checked");
+
+  const loginGrtr = G$(firstname, lastname);  // create new obj
+  
+  $("#logindiv").hide();
+
+  loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', formality).log();
+
+});
--- greetr.js
+++ greetr.js
@@ -1,95 +1,123 @@
-(function (windowObj, $) {
+;(function (windowObj, $) {
+	// PRIVATE
 
-  // PRIVATE
+	const supportedLanguages = ['en', 'es'];
 
-  const supportedLanguages = ['en', 'es'];
+	const greetings = {
+		en: 'Hello',
+		es: 'Hola'
+	};
 
-  const greetings = {
-    en: 'Hello',
-    es: 'Hola'
-  };
+	const formalGreetings = {
+		en: 'Greetings',
+		es: 'Saludos'
+	};
 
-  const formalGreetings = {
-    en: 'Greetings',
-    es: 'Saludos'
-  };
-
-  const logMessages = {
-    en: 'Logged in',
-    es: 'Inicio sesion'
-  };
-
-
-  // PUBLIC
-
-  const Greetr = function (firstName, lastName, language) {
-    return new Greetr.init(firstName, lastName, language);
-  }
-
-  // constructor
-  Greetr.init = function(firstName, lastName, language) {
-    const main = this;  // points to empty obj created by 'new' keyword
-
-    main.firstName = firstName || "",
-    main.lastName = lastName || "",
-    main.language = language || "en";
-  }
-
-  // prototype; clean way to reference vs using Greeter.init.prototype)
-  Greetr.prototype = {
-    fullName: function() {
-      return this.firstName + " " + this.lastName;
-    },
-    validate: function() {
-      if (supportedLanguages.indexOf(this.language) === -1) {
-        throw "Invalid language";
-      }
-    },
-    greeting: function() {
-      return greetings[this.language] + ", " + this.firstName + "!";
-    },
-    formalGreeting: function() {
-      return formalGreetings[this.language] + ", " + this.fullName() + "!";
-    },
-
-    // .greet logs appropriate greeting to console
-    greet: function(formal) {
-      let msg;
-
-      // if undefined or null, it will be coerced to false
-      if (formal) {
-        msg = this.formalGreeting();
-      } else {
-        msg = this.greeting();
-      }
-
-      if (console) {
-        console.log(msg);
-      }
-
-      return this;  // make method chainable; 'this' refers to calling object at execution time
-    },
-
-    // to manually check something is logged
-    log: function() {
-      if (console) {  // IE has no console obj if its console is closed
-        console.log(logMessages[this.language] + ": " + this.fullName());
-      }
-      return this;  // make chainable
-    },
-
-    // change language on the fly
-    setLang: function(language) {
-      this.language = language;
-
-      this.validate();
-      return this;  // make chainable
-    }
-
-  };
+	const logMessages = {
+		en: 'Logged in',
+		es: 'Inicio sesion'
+	};
 
 
-  Greetr.init.prototype = Greetr.prototype;  // redirect prototype of objects created by init() to Greetr.prototype
+	// PUBLIC
+
+	// entry point (returns an object)
+	const Greetr = function (firstName, lastName, language) {
+		return new Greetr.init(firstName, lastName, language);
+	}
+
+	// constructor
+	Greetr.init = function(firstName, lastName, language) {
+		const main = this;  // points to empty obj created by 'new' keyword
+
+		main.firstName = firstName || "",
+		main.lastName = lastName || "",
+		main.language = language || "en";
+
+		main.validate();
+	}
+
+
+	Greetr.prototype = {
+		fullName: function() {
+		return this.firstName + " " + this.lastName;
+		},
+		validate: function() {
+		if (supportedLanguages.indexOf(this.language) === -1) {
+			throw "Invalid language";
+		}
+		},
+		greeting: function() {
+		return greetings[this.language] + ", " + this.firstName + "!";
+		},
+		formalGreeting: function() {
+			console.log('fullname: ', this.fullName());
+		return formalGreetings[this.language] + ", " + this.fullName() + "!";
+		},
+		login: function(name) {
+		const greeting = greetings[this.language] + ", " + name + "!";
+		return $("#greeting").html(greeting);
+		},
+
+    
+		// .greet logs appropriate greeting to console
+		greet: function(formal) {
+		let msg;
+
+		// if undefined or null, it will be coerced to false
+		if (formal) {
+			msg = this.formalGreeting();
+		} else {
+			msg = this.greeting();
+		}
+
+		if (console) {
+			console.log(msg);
+		}
+
+		return this;  // make method chainable; 'this' refers to calling object at execution time
+		},
+
+		// to manually check something is logged
+		log: function() {
+			if (console) {  // IE has no console obj if its console is closed
+				console.log(logMessages[this.language] + ": " + this.fullName());
+			}
+			return this;  // make chainable
+		},
+
+		// change language on the fly
+		setLang: function(language) {
+			this.language = language;
+
+			this.validate();
+			return this;  // make chainable
+		},
+
+		// gets appropriate greeting and places in HTML element
+		HTMLGreeting: function(selector, formal) {
+			if (!$) {
+				throw 'jQuery not loaded';
+			}
+
+			if (!selector) {
+				throw 'Missing jQuery selector';
+			}
+
+			if (formal) {
+				msg = this.formalGreeting(firstname, lastname);
+			} else {
+				msg = this.greeting();
+			}
+
+			$(selector).html(msg);
+			return this;
+		}
+	};
+
+  // redirect prototype of objects created by init() to Greetr.prototype
+  // prototype is a clean way to reference vs using Greeter.init.prototype)
+  Greetr.init.prototype = Greetr.prototype;
 
   windowObj.Greetr = windowObj.G$ = Greetr;  // attach/expose Greetr to window object for anyones use (add G$ as alias)
 
